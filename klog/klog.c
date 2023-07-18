856d5f7a7336cc11613e83f17377b8df3d4b42a2
kyosold <kyosold@gmail.com>
2023-07-18 11:22:38 +0800

    添加plus,为解决在信号函数中使用

--- klog/klog.c
+++ klog/klog.c
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include "klog.h"
 
 static int klog_level = KLOG_INFO;
 static char klog_sid[128] = {0};
 static char klog_ident[512] = {0};
-static char klog_file[1024] = {0};
 static char *klog_priority[] = {
     "EMERG",
     "ALERT",
@@ -17,16 +19,14 @@ static char *klog_priority[] = {
     "WARNING",
     "NOTICE",
     "INFO",
-    "DEBUG"
-};
+    "DEBUG"};
 
-#if defined(WIN32) || defined(_WIN32) 
-#else
 static int klog_facility = LOG_USER;
-static int klog_opt = LOG_PID | LOG_NDELAY;
+static int klog_opt = LOG_PID;
 
+// --- Use syslog of linux ---
 void _klog_write(int level, const char *file, int line,
-                const char *fun, const char *fmt, ...)
+                 const char *fun, const char *fmt, ...)
 {
     if (level > klog_level)
         return;
@@ -46,7 +46,7 @@ void _klog_write(int level, const char *file, int line,
 
 /**
  * @brief open log
- * 
+ *
  * @param ident     the name of program
  * @param facility  LOG_MAIL/LOG_USER
  * @param level     KLOG_DEBUG/KLOG_INFO ...
@@ -60,47 +60,92 @@ void klog_open(const char *ident, int facility, int level, char *sid)
     snprintf(klog_sid, sizeof(klog_sid), "%s", sid);
 }
 
-#endif
+// --- Use write to syslog file ---
+static char klog_file[1024] = {0};
+static char klog_hostname[1024];
+void klog_open_plus(const char *ident, int facility, int level, char *sid)
+{
+    switch (facility)
+    {
+    case LOG_MAIL:
+        snprintf(klog_file, sizeof(klog_file), "/var/log/maillog");
+        break;
+    case LOG_AUTHPRIV:
+        snprintf(klog_file, sizeof(klog_file), "/var/log/secure");
+        break;
+    case LOG_CRON:
+        snprintf(klog_file, sizeof(klog_file), "/var/log/cron");
+        break;
+    case LOG_DAEMON:
+        snprintf(klog_file, sizeof(klog_file), "/var/log/maillog");
+        break;
+    case LOG_NEWS:
+    case LOG_UUCP:
+        snprintf(klog_file, sizeof(klog_file), "/var/log/spooler");
+        break;
+    case LOG_FTP:
+    case LOG_KERN:
+    case LOG_LPR:
+    case LOG_SYSLOG:
+    case LOG_USER:
+    case LOG_AUTH:
+    default:
+        snprintf(klog_file, sizeof(klog_file), "/var/log/messages");
+        break;
+    }
 
+    snprintf(klog_ident, sizeof(klog_ident), "%s", ident);
+    klog_level = level;
+    snprintf(klog_sid, sizeof(klog_sid), "%s", sid);
+    gethostname(klog_hostname, sizeof(klog_hostname));
+    char *end = (char *)memchr(klog_hostname, '.', strlen(klog_hostname));
+    if (end)
+        *end = 0;
+}
 
-
-
-
-void _klog_write_file(int level, const char *fname, int line,
+void _klog_write_file(int level, const char *file, int line,
                       const char *fun, const char *fmt, ...)
 {
     if (level > klog_level)
         return;
 
-    FILE *fp;
-    time_t local_time;
-    char time_str[128];
-    char klog_line_str[KLOG_MAX_LINE];
+    // get datetime. e.g: Jul 13 15:06:01
+    char log_date[128];
+    time_t now = time(NULL);
+    struct tm *local_tm = localtime(&now);
+    strftime(log_date, sizeof(log_date), "%b %d %H:%M:%S", local_tm);
 
-    // 获取本地时间
-    time(&local_time);
-    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&local_time));
+    // make log line prefix. e.g: Jul 13 16:38:45 songjian test[6857]: [INFO] test.c:35:main
+    char pref[KLOG_MAX_LINE];
+    const char *pref_fmt = "%s %s %s[%d]: [%s] %s:%d:%s %s";
+    snprintf(pref, sizeof(pref), pref_fmt, log_date, klog_hostname, klog_ident, getpid(), klog_priority[level], file, line, fun, klog_sid);
 
-    // 日志内容格式转换
-    char new_fmt[KLOG_MAX_LINE];
-    const char *head_fmt = "[%s] %s:%d:%s %s %s";
-    snprintf(new_fmt, sizeof(new_fmt), head_fmt, klog_priority[level], fname, line, fun, klog_sid, fmt);
-
+    // make log muli-arguments
+    char log_line[KLOG_MAX_LINE];
     va_list ap;
     va_start(ap, fmt);
-    vsnprintf(klog_line_str, sizeof(klog_line_str), fmt, ap);
+    vsnprintf(log_line, sizeof(log_line), fmt, ap);
     va_end(ap);
 
-    // 打开日志
-    fp = fopen(klog_file, "a");
-    if (fp != NULL) {
-        fprintf(fp, "%s [%s:%d:%s]: [%s] sid:%s %s\n",
-                time_str, fname, line, fun, klog_priority[level], klog_sid, klog_line_str);
+    FILE *fp = fopen(klog_file, "a+");
+    if (fp != NULL)
+    {
+        fprintf(fp, "%s %s\n", pref, log_line);
         fclose(fp);
     }
+
+    // // final log
+    // char new_fmt[KLOG_MAX_LINE];
+    // const char *head_fmt = "%s %s\n";
+    // snprintf(new_fmt, sizeof(new_fmt), head_fmt, pref, log_line);
+
+    // int fd = open(klog_file, O_WRONLY | O_NDELAY | O_APPEND | O_CREAT, 0644);
+    // if (fd == -1)
+    //     return;
+    // write(fd, new_fmt, strlen(new_fmt));
+    // close(fd);
+    return;
 }
-
-
 
 void klog_open_file(const char *file, const char *ident, int level, char *sid)
 {
@@ -108,11 +153,15 @@ void klog_open_file(const char *file, const char *ident, int level, char *sid)
     snprintf(klog_ident, sizeof(klog_ident), "%s", ident);
     klog_level = level;
     snprintf(klog_sid, sizeof(klog_sid), "%s", sid);
+    gethostname(klog_hostname, sizeof(klog_hostname));
+    char *end = (char *)memchr(klog_hostname, '.', strlen(klog_hostname));
+    if (end)
+        *end = 0;
 }
 
 /**
  * @brief set log level
- * 
+ *
  * @param level     KLOG_DEBUG/KLOG_INFO ...
  */
 void klog_set_level(int level)
@@ -121,7 +170,7 @@ void klog_set_level(int level)
 }
 /**
  * @brief set log session id
- * 
+ *
  * @param sid       using tag session
  */
 void klog_set_sid(char *sid)
--- klog/klog.h
+++ klog/klog.h
@@ -1,9 +1,7 @@
 #ifndef __KLOG_H__
 #define __KLOG_H__
-#if defined(WIN32) || defined(_WIN32) 
-#else
+
 #include <syslog.h>
-#endif
 
 #define KLOG_DEBUG 7
 #define KLOG_INFO 6
@@ -16,14 +14,12 @@
 
 #define KLOG_MAX_LINE 4096
 
-void klog_open(const char *ident, int facility, int level, char *sid);
-void klog_open_file(const char *file, const char *ident, int level, char *sid);
 void klog_set_level(int level);
 void klog_set_sid(char *sid);
 
+// --- Use syslog of linux ---
+void klog_open(const char *ident, int facility, int level, char *sid);
 void _klog_write(int level, const char *file, int line, const char *fun, const char *fmt, ...);
-void _klog_write_file(int level, const char *fname, int line, const char *fun, const char *fmt, ...);
-
 #define klog_debug(fmt, args...) \
     _klog_write(KLOG_DEBUG, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
 #define klog_info(fmt, args...) \
@@ -41,7 +37,29 @@ void _klog_write_file(int level, const char *fname, int line, const char *fun, c
 #define klog_emerg(fmt, args...) \
     _klog_write(KLOG_EMERG, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
 
+// --- Use write to syslog file ---
+void klog_open_plus(const char *ident, int facility, int level, char *sid);
+void _klog_write_plus(int level, const char *file, int line,
+                      const char *fun, const char *fmt, ...);
+#define klog_debug_plus(fmt, args...) \
+    _klog_write_file(KLOG_DEBUG, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
+#define klog_info_plus(fmt, args...) \
+    _klog_write_file(KLOG_INFO, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
+#define klog_notice_plus(fmt, args...) \
+    _klog_write_file(KLOG_NOTICE, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
+#define klog_warningv(fmt, args...) \
+    _klog_write_file(KLOG_WARNING, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
+#define klog_error_plus(fmt, args...) \
+    _klog_write_file(KLOG_ERROR, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
+#define klog_crit_plus(fmt, args...) \
+    _klog_write_file(KLOG_CRIT, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
+#define klog_alert_plus(fmt, args...) \
+    _klog_write_file(KLOG_ALERT, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
+#define klog_emerg_plus(fmt, args...) \
+    _klog_write_file(KLOG_EMERG, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
 
+// -- Use custom log file ---
+void klog_open_file(const char *file, const char *ident, int level, char *sid);
 #define klog_debug_file(fmt, args...) \
     _klog_write_file(KLOG_DEBUG, __FILE__, __LINE__, __FUNCTION__, fmt, ##args)
 #define klog_info_file(fmt, args...) \
